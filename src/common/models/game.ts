c72d98a8582c29d356f50b59eb25f3c2a76f058e
talpasco <talpasco@gmail.com>
2021-04-29 18:38:02 +0300

    update game - union all players of the game

--- src/common/models/game.ts
+++ src/common/models/game.ts
@@ -1,15 +1,21 @@
 "use strict";
 const LoopBackContext = require("loopback-context");
 const async = require("async");
-import shortid from "shortid";
 module.exports = function (game) {
-  game.observe("before save", (ctx, next) => {
-    if (!ctx.instance) {
-      // create operation
-      next();
+  game.observe("before save", async (ctx, next) => {
+    let instance = ctx.instance || ctx.data;
+    if (ctx.isNewInstance) {// create operation
+      let curPlayer = instance.players.length > 0 ? instance.players[0] : null;
+      if (!curPlayer)
+        return next(new Error("Atleast 1 player is needed"));
+      instance.currentFrame = 1;
+      instance.currentPlayer = instance.currentPlayer || curPlayer;
+      instance.startingPlayer = instance.startingPlayer || curPlayer;
     } else {
-      //const userId = ctx.instance.userID;
-      ctx.instance.id = shortid.generate();
+      let initialPlayers = await ctx.Model.find(ctx.where).players;
+      let allPlayers = [...new Set([...initialPlayers, ...instance.players])];
+      instance.players = allPlayers; // union all players of the game
     }
+    next();
   });
 };
